fc4d7f0472f5538f47c38bf5fd2f4f2d5704479e
PranavPurwar <purwarpranav80@gmail.com>
2023-09-27 20:24:40 +0530

    Set AGP to v8.1.1
    Signed-off-by: PranavPurwar <purwarpranav80@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.library") version "8.3.0-alpha05" apply false
+    id("com.android.library") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20-Beta2" apply false
 }
